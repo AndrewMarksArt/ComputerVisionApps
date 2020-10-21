384c9f39338ce99425c782da0e19cf3231b5c705
AndrewMarksArt <andrew.colin.marks@gmail.com>
2020-10-21 15:58:38 -0700

    added simple CNN model, train model and print out classification report along with ploting the models performance.

--- Keras_Basics_Getting_Started/train_simple_nn.py
+++ Keras_Basics_Getting_Started/train_simple_nn.py
@@ -57,3 +57,56 @@ for imagePath in imagePaths:
 # scale pixel intensities from 0-255 to 0-1
 data = np.array(data, dtype="float") / 255.0
 labels = np.array(labels)
+
+# split the data into training and testing sets, 75/25 split
+(X_train, X_test, y_train, y_test) = train_test_split(data, label,
+                                                      test_size=0.25,
+                                                      random_state=42)
+
+# convert the labels from integers to vectors
+lb = LabelBinarizer()
+y_train = lb.fit_transform(y_train)
+y_test = lb.fit_transform(y_test)
+
+# very simple model -> input layer, 2 hidden layers, output layer
+model = Sequential()
+model.add(Dense(1024, input_shape=(3072, ), activation="sigmoid"))
+model.add(Dense(512, activation="sigmoid"))
+model.add(Dense(len(lb.classes_), activation="softmax"))
+
+# compile the model
+# initialize learning rate and # of epochs
+INIT_LR = 0.01
+EPOCHS = 80
+
+# use SGD as our optimizer and categorical cross-entropy loss
+print("[INFO] training network...")
+opt = SGD(lr=INIT_LR)
+model.compile(loss="categorical_crossentropy", optimizer=opt,
+              metrics=["accuracy"])
+
+# train the model
+H = model.fit(x=X_train, y=y_train, validation_data=(X_test, y_test),
+              epochs=EPOCHS, batch_size=32)
+
+# model evaluation
+print("[INFO] evaluating the model...")
+predictions = model.predict(x=X_test, batch_size=32)
+print(classification_report(y_test.argmax(axis=1),
+                            predictions.argmax(axis=1),
+                            target_names=lb.classes_))
+
+# plot the training loss and accuracy
+N = np.arange(0, EPOCHS)
+plt.style.use("ggplot")
+plt.figure()
+plt.plot(N, H.history["loss"], label="training_loss")
+plt.plot(N, H.history["val_loss"], label="validation_loss")
+plt.plot(N, H.history["accuracy"], label="accuracy")
+plt.plot(N, H.history["val_accuracy"], label="validation_accuracy")
+plt.title("Training Loss and Accuracy for Simple CNN")
+plt.xlabel("Epoch #")
+plt.ylabel("Loss/Accuracy")
+plt.legend()
+plt.savefig(args["plot"])
+
